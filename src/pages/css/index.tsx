c25ca2de8dfee5b4b06e4d129504c9f82e6260f4
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-07-11 23:52:22 +0900

    update: CSSPage

--- src/pages/css/index.tsx
+++ src/pages/css/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
 
 import './page.css'
+import { useState } from 'react'
 
 export default function CSSPage() {
+  const [bgClassName, setBgClassName] = useState<'red' | 'blue'>('red')
+
+  const toggleBgClassName = () => {
+    setBgClassName(bgClassName === 'red' ? 'blue' : 'red')
+  }
+
   return (
     <div className={'page'}>
-      <div className={'card'}>
+      <div className={`card ${bgClassName}`} onClick={toggleBgClassName}>
         <p>Hello, CSS</p>
       </div>
     </div>
