0f2a3bd8718d22609874cfdfff40a58b1825dbd7
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-07-12 00:25:20 +0900

    update: add toggle functionality for background color

--- src/pages/chakra/index.tsx
+++ src/pages/chakra/index.tsx
@@ -3,10 +3,16 @@ import * as React from 'react'
 import { Box, Center, ChakraProvider, Text } from '@chakra-ui/react'
 
 export default function ChakraPage() {
+  const [bg, setBg] = React.useState<'red' | 'blue'>('red')
+
+  const toggleBg = () => {
+    setBg(bg === 'red' ? 'blue' : 'red')
+  }
+
   return (
     <ChakraProvider>
       <Center h={'100vh'}>
-        <Box bg={'teal.500'} px={10} py={8} rounded={'md'}>
+        <Box bg={bg} px={10} py={8} rounded={'md'} onClick={toggleBg}>
           <Text color={'white'} fontWeight={'bold'} fontSize={'xl'}>
             Hello, Chakra UI
           </Text>
